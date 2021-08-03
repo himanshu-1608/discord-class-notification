60577cc39a081c2fee3912790b4eb285af67191d
Himanshu Yadav <ydv1608@gmail.com>
2021-08-03 23:17:04 +0530

    Changed JSON Schema, Changed env variable calls from class object to dict

--- index.py
+++ index.py
@@ -4,9 +4,24 @@ import json
 
 # Global constans to be declared here
 
-DATABASE_FOLDER = "./data"
+DATABASE_LOCATION = "./data"
 TOP_LEVEL_SECTIONS = ["IT_A", "IT_B"]
 CURR_TIME = datetime.now()
 CURR_DAY = CURR_TIME.strftime("%A")  # current day
 CURR_HOUR = CURR_TIME.strftime("%H")  # current hour
 CURR_MIN = CURR_TIME.strftime("%M")  # current min
+
+# Code starts here
+
+for section in TOP_LEVEL_SECTIONS:
+    # Don't process IT_A for now
+    if section == "IT_A":
+        continue
+
+    daily_schedule_filename = "{db_location}/{section}/{day}.json".format(
+        db_location=DATABASE_LOCATION, section=section, day=CURR_DAY
+    )
+    daily_schedule_file = open(daily_schedule_filename, "r")
+    daily_schedule = json.load(daily_schedule_file)
+    for event in daily_schedule:
+        print(event["type"])
