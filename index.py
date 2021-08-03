d831839e3bddc11d7622ac0c06c5d09888f4ee1c
Himanshu Yadav <ydv1608@gmail.com>
2021-08-04 01:25:16 +0530

    Added feature webhook requests

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 import json
+import requests
 
 # package imports
 from config import ENV
@@ -28,20 +29,13 @@ def get_daily_schedule(section):
     return json.load(daily_schedule_file)
 
 
-def is_event_close(event):
-    start_time = EVENT_SLOTS[event["start_time"]]["start_time"]
-    event_start_time = datetime.strptime(CURR_DATE + " " + start_time, "%d-%m-%Y %H:%M")
-    time_diff = event_start_time - CURR_TIME
-    return 0 <= time_diff.total_seconds() <= 300
-
-
 def prepare_payload(event, section_index):
     role_tag = "ROLE_{section}".format(section=event["sections"][section_index])
     section_role_tag = ENV[role_tag]
     event_name = " **{event_short_name}** ".format(event_short_name=event["short_name"])
     links = ""
     for index in range(len(event["faculty_names"])):
-        links += "{faculty_name} : {meet_link}\n".format(
+        links += "{faculty_name} : <{meet_link}>\n".format(
             faculty_name=event["faculty_names"][index],
             meet_link=event["meet_links"][index],
         )
@@ -55,7 +49,14 @@ def send_alert(event):
             section=event["sections"][section_index]
         )
         webhook_url = ENV[webhook_name]
-        print(webhook_url)
+        requests.post(webhook_url, data={"content": payload})
+
+
+def is_event_close(event):
+    start_time = EVENT_SLOTS[event["start_time"]]["start_time"]
+    event_start_time = datetime.strptime(CURR_DATE + " " + start_time, "%d-%m-%Y %H:%M")
+    time_diff = event_start_time - CURR_TIME
+    return 0 <= time_diff.total_seconds() <= 300
 
 
 def event_handler(event):
