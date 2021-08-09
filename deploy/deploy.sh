d2e403f8439f6a3fe6b405c82fe5a1cec9da4c0d
Himanshu Yadav <ydv1608@gmail.com>
2021-08-09 11:17:46 +0530

    pipefix48

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
-
-# any future command that fails will exit the script
 set -e
-# Lets write the public key of our aws instance
-eval $(ssh-agent -s)
-# echo "$2" | ssh-add - > /dev/null
-# touch ~/.ssh/id_rsa
-# echo $PRIVATE_KEY > ~/.ssh/id_rsa
-# chmod 600 ~/.ssh/id_rsa
-
 # disable the host key checking.
 chmod +x ./deploy/disableHostKeyChecking.sh
 ./deploy/disableHostKeyChecking.sh
-
-# we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
-# comma seperated values of ip addresses.
-DEPLOY_SERVERS=$1
+DEPLOY_SERVERS=$DEPLOY_SERVERS
 # lets split this string and convert this into array
 # In UNIX, we can use this commond to do this
 # ${string//substring/replacement}
 # our substring is "," and we replace it with nothing.
 ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
 
-# echo $2 > himanshu-ssh-keypair.pem
-
-# chmod 400 himanshu-ssh-keypair.pem
-chmod 400 id_rsb
-# echo $(<himanshu-ssh-keypair.pem)
-ls
+chmod 400 $PEM_FILE_PATH
 
 for server in "${ALL_SERVERS[@]}"
 do
-  echo "deploying to ${server}" # | sed 's/./& /g'
-  # ssh -i ./ssh_key.pem ubuntu@${server} 'bash -s' < ./deploy/updateAndRestart.sh
-  ssh -i id_rsb ubuntu@${server} 'bash -s' < ./deploy/updateAndRestart.sh
+  echo "deploying to ${server}"
+  ssh -i $PEM_FILE_PATH ubuntu@${server} 'bash -s' < ./deploy/updateAndRestart.sh
 done
